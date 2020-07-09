858e9f767a7df90491a829ded1e0bcdf9088deb5
Sophie <sophiehickey123@gmail.com>
2020-07-09 20:44:16 +0000

    added get & post methods to register

--- app.py
+++ app.py
@@ -14,10 +14,28 @@ app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 mongo = PyMongo(app)
 
-
 @app.route("/")
-def test():
-    return "Hello world"
+@app.route("/index")
+def index():
+    return render_template('pages/index.html', page_title="Home")
+
+
+@app.route("/login")
+def login():
+    return render_template('pages/login.html', page_title="Login")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template('pages/register.html', page_title="Register")
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True) 
