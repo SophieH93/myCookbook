a9e198210c915597d6b3203297dae4bd5a42b000
Sophie <sophiehickey123@gmail.com>
2020-07-12 15:42:49 +0000

    ammended class name and removed prep-time

--- templates/forms.py
+++ templates/forms.py
@@ -3,13 +3,12 @@ from wtforms import StringField, SubmitField, TextAreaField, IntegerField, Selec
 from wtforms.validators import DataRequired, Length, EqualTo
 
 
-class addRecipe(FlaskForm):
+class addRecipeForm(FlaskForm):
     recipe_name = StringField('Recipe Name', 
                                 validators=[DataRequired()])
     recipe_description = TextAreaField('Recipe Description',
                                 validators=[DataRequired(), Length(min=3, max=20)])                        
     cooking_time = IntegerField('Cooking Time (min)', validators=[DataRequired()])
-    prep_time = IntegerField('Prep Time', validators=[DataRequired()])
     category = SelectField('Category')
     ingredients = TextAreaField('Ingredients',
                                 validators=[DataRequired()])
