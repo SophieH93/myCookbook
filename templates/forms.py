a86cfcfe7a2423956d5100e8e43fb2c71379206d
Sophie <sophiehickey123@gmail.com>
2020-07-12 13:59:48 +0000

    imported Submitform/added to description

--- templates/forms.py
+++ templates/forms.py
@@ -1,14 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField, SelectField
+from wtforms import StringField, SubmitField, TextAreaField, IntegerField, SelectField
 from wtforms.validators import DataRequired, Length, EqualTo
 
 
-class addRecipe(FlaskForm)
+class addRecipe(FlaskForm):
     recipe_name = StringField('Recipe Name', 
-                                validators=[DataRequired(), Length(min=3, max=20)])
-    recipe_description = StringField('Recipe Description',
+                                validators=[DataRequired()])
+    recipe_description = TextAreaField('Recipe Description',
                                 validators=[DataRequired(), Length(min=3, max=20)])                        
-    cooking_time = IntegerField('Cooking Time', validators=[DataRequired()])
+    cooking_time = IntegerField('Cooking Time (min)', validators=[DataRequired()])
     prep_time = IntegerField('Prep Time', validators=[DataRequired()])
     category = SelectField('Category')
     ingredients = TextAreaField('Ingredients',
