c9e3cb2ec0a758070d006d8f3d145fa16ed62ce4
Sophie <sophiehickey123@gmail.com>
2020-07-22 19:02:01 +0000

    tidied format of code

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
-from flask import Flask, render_template, url_for, session, redirect, request, flash
+from flask import Flask, render_template, url_for, \
+    session, redirect, request, flash
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-from templates.forms import addRecipeForm
+from forms import addRecipeForm
 import bcrypt
 
 if os.path.exists("env.py"):
@@ -10,118 +11,67 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
+"""MAKE THESE VARIABLES"""
 app.config["MONGO_DBNAME"] = "myCookbook"
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
 mongo = PyMongo(app)
 
-
-'''
-Home page
-'''
 @app.route("/")
-@app.route("/index")    
-def index():
-    '''
-    When page loads, index page is displayed.
-    '''
-    return render_template('pages/index.html')
-
-'''
-Login page
-'''
-@app.route("/login", methods=["GET", "POST"])
-def login():   
-    if request.method == "POST":
-        users = mongo.db.users
-        login_user = users.find_one({'name' : request.form['username']})
-        if login_user:
-            if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['password']) == login_user['password']:
-                session['username'] = request.form['username']      
-                return redirect(url_for('index'))
-        flash('Invalide username/password !')        
-    return render_template('pages/login.html')
-
-'''
-Register page
-'''
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    if request.method == "POST":
-        users = mongo.db.users
-        existing_user = users.find_one({'name': request.form['username']})
-      #  flash('You have been successfully Registered!')
-        if existing_user is None:
-            hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
-            users.insert({'name' : request.form['username'], 'password' : hashpass})
-            session['username'] = request.form['username']
-            return redirect(url_for('index'))
-        flash('That username already exists!')
-    return render_template('pages/register.html')
+@app.route("/home")
+def home():
+    """
+    When page loads, home page is displayed.
+    """
+    return render_template('pages/home.html')
 
 
-'''
-Recipe page
-'''
 @app.route('/recipes')
 def recipes():
-
-    return render_template('pages/recipes.html', recipes=mongo.db.recipes.find())
-
-'''
-Add Recipe 
-'''
-@app.route('/add_recipe')   
-def add_recipe():
-    '''
-    The function calls the addRecipeForm class from forms.py
-    to display the form for adding a recipe.
-    '''
-    form = addRecipeForm()
-    return render_template('pages/add_recipe.html', form=form)
-
-
-'''
-Insert Recipe to Database
-'''
-@app.route("/insert_recipe", methods=["GET", "POST"])
-def insert_recipe():       
     """
-    Add the user's data to the MongoDB and redirect
-    the user to Recipe page.
+    Displays users recipes
     """
-    recipes = mongo.db.recipes 
+    return render_template('pages/recipes.html',
+                            recipes=mongo.db.recipes.find())
 
-    recipes.insert_one( {
-        "recipe_name": request.form.get("recipe_name"),
-        "description": request.form.get("recipe_description"),
-        "prep_time": request.form.get("prep_time"),
-        "cooking_time": request.form.get("cooking_time"),
-        "category": request.form.get("category"),
-        "ingredients": request.form.get('ingredients'),
-        "steps": request.form.get('steps'),
-        "image": request.form.get("image")
-    })
-    
-    return redirect(url_for('recipes'))
 
-'''
-Edit Recipe
-'''
-@app.route("/edit_recipe/<recipe_id>")
-def edit_recipe(recipe_id):
-
-    form = addRecipeForm()
+@app.route('/single_recipe_info/<recipe_id>')
+def single_recipe_info(recipe_id):
+    '''
+    Displays info about a selected recipe.
+    '''
     selected_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
-    return render_template('pages/edit_recipe.html',  selected_recipe=selected_recipe,  form=form)
+    return render_template("pages/single_recipe_info.html",
+                            selected_recipe=selected_recipe)
+   
 
-'''
-Update the Recipe in the database
-'''
-@app.route("/update_recipe/<recipe_id>", methods=["POST"])
+@app.route('/add/recipe', methods=["GET", "POST"]) 
+def insert_recipe():
+  
+    if request.method == "POST":
+        recipes = mongo.db.recipes 
+
+        recipes.insert_one({
+            "recipe_name": request.form.get("recipe_name"),
+            "description": request.form.get("recipe_description"),
+            "prep_time": request.form.get("prep_time"),
+            "cooking_time": request.form.get("cooking_time"),
+            "category": request.form.get("category"),
+            "ingredients": request.form.get('ingredients'),
+            "steps": request.form.get('steps'),
+            "image": request.form.get("image")
+        })
+        return redirect(url_for('recipes'))
+    else:
+        form = addRecipeForm()
+        return render_template('pages/add-recipe.html', form=form)
+
+@app.route("/edit/recipe/<recipe_id>", methods=["POST"])
 def update_recipe(recipe_id):
+    """
+    Update the Recipe in the database
+   """
     recipes = mongo.db.recipes
-
     selected_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
 
     if request.method == "POST":
@@ -135,42 +85,54 @@ def update_recipe(recipe_id):
             "steps": request.form.get('steps'),
             "image": request.form.get("image")
         })
+        return redirect(url_for('single_recipe_info', recipe_id=recipe_id))
+    else:
+        form = addRecipeForm()
+        return render_template('pages/edit-recipe.html',
+                                selected_recipe=selected_recipe, form=form)
 
-    return redirect(url_for('single_recipe_info', recipe_id=recipe_id))
-
-'''
-Single Recipe 
-'''
-@app.route('/single_recipe_info/<recipe_id>')
-def single_recipe_info(recipe_id):
-    '''
-    Displays info about a selected recipe.
-    '''
-    selected_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
-    return render_template("pages/single_recipe_info.html", selected_recipe=selected_recipe)
-
-
-'''
-Delete Recipe 
-'''
-@app.route('/delete_recipe/<recipe_id>')
-def delete_recipe(recipe_id):
+@app.route('/delete-recipe/<recipe_id>')
+def deleteRecipe(recipe_id):
     '''
     Delete Recipe from the Database
     '''
     mongo.db.recipes.remove({'_id': ObjectId(recipe_id)}) 
     return redirect(url_for('recipes'))
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        users = mongo.db.users
+        login_user = users.find_one({'name' : request.form['username']})
+        if login_user:
+            if bcrypt.hashpw(request.form['password'].encode('utf-8'),
+                            login_user['password']) == login_user['password']:
+                session['username'] = request.form['username']
+                return redirect(url_for('index'))
+        flash('Invalide username/password !')
+    return render_template('pages/login.html')
 
-'''
-Logout
-'''
-# Logout
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        users = mongo.db.users
+        existing_user = users.find_one({'name': request.form['username']})
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), 
+                                    bcrypt.gensalt())
+            users.insert({'name' : request.form['username'],
+                        'password' : hashpass})
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        flash('That username already exists!')
+    return render_template('pages/register.html')
+
+
 @app.route('/logout')
 def logout():
+
     session.pop("username",  None)
     return redirect(url_for("index"))
 
-
 if __name__ == '__main__':
-    app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=False) 
+    app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
