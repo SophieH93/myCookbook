8afd0c1989a100d7f04507197ff6cf98cea6ef55
Sophie <sophiehickey123@gmail.com>
2020-07-29 18:13:36 +0000

    added env file

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ.setdefault("MONGO_URI", "mongodb+srv://c00k:b00k@mycookbook.fa4gc.mongodb.net/myCookbook?retryWrites=true&w=majority" )
+os.environ["SECRET_KEY"] = "secrey_key"
