e9896000d1488d86f557a492d631a2a776aae32f
Sophie <sophiehickey123@gmail.com>
2020-08-01 12:19:35 +0000

    deleted env and changed info

--- env.py
+++
@@ -1,4 +0,0 @@
-import os
-
-os.environ.setdefault("MONGO_URI", "mongodb+srv://bookCook:myb0ok@mycookbook.fa4gc.mongodb.net/myCookbook?retryWrites=true&w=majority" )
-os.environ["SECRET_KEY"] = "secret_key"
