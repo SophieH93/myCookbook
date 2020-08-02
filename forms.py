539e2c7c0c6812bca6c4d0f00a303c0d94ac6c8b
Sophie <sophiehickey123@gmail.com>
2020-08-02 17:53:01 +0000

    Python Formatter

--- forms.py
+++ forms.py
@@ -4,12 +4,16 @@ from wtforms.validators import DataRequired, Length, EqualTo
 
 
 class addRecipeForm(FlaskForm):
-    recipe_name = StringField('Recipe Name', validators=[DataRequired()])
-    recipe_description = TextAreaField('Recipe Description', validators=[DataRequired()])
-    prep_time = IntegerField('Prep Time (min)', validators=[DataRequired()])
-    cooking_time = IntegerField('Cooking Time (min)', validators=[DataRequired()])
-    category = TextAreaField('category',
+
+    recipe_name = StringField('Recipe Name',
+                              validators=[DataRequired()])
+    recipe_description = TextAreaField('Recipe Description',
+            validators=[DataRequired()])
+    prep_time = IntegerField('Prep Time (min)',
+                             validators=[DataRequired()])
+    cooking_time = IntegerField('Cooking Time (min)',
                                 validators=[DataRequired()])
+    category = TextAreaField('category', validators=[DataRequired()])
     ingredients = TextAreaField('Ingredients',
                                 validators=[DataRequired()])
     steps = TextAreaField('Steps', validators=[DataRequired()])
